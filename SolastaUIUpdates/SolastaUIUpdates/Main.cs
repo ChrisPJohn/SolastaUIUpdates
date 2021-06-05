ec0c93fe0fdf58447d08dd063217c2858cd3a764
leprechaun7377 <chris.p.john@gmail.com>
2021-06-05 01:34:51 -0700

    Initial version of UI updates mod. New feature is allowing powers to show on multiple lines.

---
+++ SolastaUIUpdates/SolastaUIUpdates/Main.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+using I2.Loc;
+
+namespace SolastaUIUpdates
+{
+    public class Main
+    {
+        public static readonly string MOD_FOLDER = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        [Conditional("DEBUG")]
+        internal static void Log(string msg) => Logger.Log(msg);
+        internal static void Error(Exception ex) => Logger?.Error(ex.ToString());
+        internal static void Error(string msg) => Logger?.Error(msg);
+        internal static void Warning(string msg) => Logger?.Warning(msg);
+        internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
+
+        internal static void LoadTranslations()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(MOD_FOLDER);
+            FileInfo[] files = directoryInfo.GetFiles($"Translations-??.txt");
+
+            foreach (var file in files)
+            {
+                var filename = Path.Combine(MOD_FOLDER, file.Name);
+                var code = file.Name.Substring(13, 2);
+                var languageSourceData = LocalizationManager.Sources[0];
+                var languageIndex = languageSourceData.GetLanguageIndexFromCode(code);
+
+                if (languageIndex < 0)
+                    Error($"language {code} not currently loaded.");
+                else
+                    using (var sr = new StreamReader(filename))
+                    {
+                        String line, term, text;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            try
+                            {
+                                var splitted = line.Split(new[] { '\t', ' ' }, 2);
+                                term = splitted[0];
+                                text = splitted[1];
+                            } catch
+                            {
+                                Error($"invalid translation line \"{line}\".");
+                                continue;
+                            }
+                            if (languageSourceData.ContainsTerm(term))
+                            {
+                                languageSourceData.RemoveTerm(term);
+                                Warning($"official game term {term} was overwritten with \"{text}\"");
+                            }
+                            languageSourceData.AddTerm(term).Languages[languageIndex] = text;
+                        }
+                    }
+            }
+        }
+
+        internal static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                Logger = modEntry.Logger;
+
+                LoadTranslations();
+
+                modEntry.OnGUI = OnGUI;
+
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+
+            return true;
+        }
+
+        internal static void ModEntryPoint()
+        {
+
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+        }
+    }
+}
---
+++ SolastaUIUpdates/SolastaUIUpdates/Patches/PowerSelectionPanelPatcher.cs
@@ -0,0 +1,81 @@
+﻿
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SolastaUIUpdates.Patches
+{
+    class PowerSelectionPanelPatcher
+    {
+        private static RectTransform secondRow;
+        private static RectTransform thirdRow;
+
+        [HarmonyPatch(typeof(PowerSelectionPanel), "Bind")]
+        internal static class PowerSelectionPanel_SecondLine
+        {
+            internal static void Postfix(PowerSelectionPanel __instance)
+            {
+                List<UsablePowerBox> powerBoxes = (List<UsablePowerBox>)Traverse.Create(__instance).Field("usablePowerBoxes").GetValue();
+                RectTransform powersTable = (RectTransform)Traverse.Create(__instance).Field("powersTable").GetValue();
+                if (powerBoxes.Count > 14)
+                {
+                    if (thirdRow == null)
+                    {
+                        thirdRow = GameObject.Instantiate(powersTable);
+                    }
+                    thirdRow.gameObject.SetActive(true);
+                    thirdRow.DetachChildren();
+                    thirdRow.SetParent(powersTable.parent.transform, true);
+                    thirdRow.transform.position = new Vector3(powersTable.transform.position.x, powersTable.transform.position.y + 200, powersTable.transform.position.z);
+                    int toStayCount = powersTable.childCount * 2 / 3;
+                    for (int i = powersTable.childCount - 1; i > toStayCount; i--)
+                    {
+                        powersTable.GetChild(i).SetParent(thirdRow, false);
+                    }
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(thirdRow);
+                }
+                if (powerBoxes.Count > 7)
+                {
+                    if (secondRow == null)
+                    {
+                        secondRow = GameObject.Instantiate(powersTable);
+                    }
+                    secondRow.gameObject.SetActive(true);
+                    secondRow.DetachChildren();
+                    secondRow.SetParent(powersTable.parent.transform, true);
+                    secondRow.transform.position = new Vector3(powersTable.transform.position.x, powersTable.transform.position.y + 80, powersTable.transform.position.z);
+                    int toStayCount = powersTable.childCount / 2;
+                    for (int i = powersTable.childCount - 1; i > toStayCount; i--)
+                    {
+                        powersTable.GetChild(i).SetParent(secondRow, false);
+                    }
+
+                    float height = __instance.transform.parent.GetComponent<RectTransform>().rect.height;
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(powersTable);
+                    __instance.RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, powersTable.rect.width);
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(secondRow);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PowerSelectionPanel), "Unbind")]
+        internal static class PowerSelectionPanel_SecondLineUnbind
+        {
+            internal static void Postfix(PowerSelectionPanel __instance)
+            {
+                if (secondRow != null && secondRow.gameObject.activeSelf)
+                {
+                    Gui.ReleaseChildrenToPool(secondRow);
+                    secondRow.gameObject.SetActive(false);
+                }
+
+                if (thirdRow != null && thirdRow.gameObject.activeSelf)
+                {
+                    Gui.ReleaseChildrenToPool(thirdRow);
+                    thirdRow.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
