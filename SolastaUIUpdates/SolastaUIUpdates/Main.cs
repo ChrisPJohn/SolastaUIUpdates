c492d6b6fd9aece8195e3e41937b0a0b2dfbd3aa
Paulo Monteiro <pauloesquilo@gmail.com>
2021-07-10 07:42:04 -0700

    clean up mod

--- SolastaUIUpdates/SolastaUIUpdates/Main.cs
+++ SolastaUIUpdates/SolastaUIUpdates/Main.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Reflection;
 using UnityModManagerNet;
 using HarmonyLib;
-using I2.Loc;
 
 namespace SolastaUIUpdates
 {
@@ -19,54 +18,11 @@ namespace SolastaUIUpdates
         internal static void Warning(string msg) => Logger?.Warning(msg);
         internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
 
-        internal static void LoadTranslations()
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(MOD_FOLDER);
-            FileInfo[] files = directoryInfo.GetFiles($"Translations-??.txt");
-
-            foreach (var file in files)
-            {
-                var filename = Path.Combine(MOD_FOLDER, file.Name);
-                var code = file.Name.Substring(13, 2);
-                var languageSourceData = LocalizationManager.Sources[0];
-                var languageIndex = languageSourceData.GetLanguageIndexFromCode(code);
-
-                if (languageIndex < 0)
-                    Error($"language {code} not currently loaded.");
-                else
-                    using (var sr = new StreamReader(filename))
-                    {
-                        String line, term, text;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            try
-                            {
-                                var splitted = line.Split(new[] { '\t', ' ' }, 2);
-                                term = splitted[0];
-                                text = splitted[1];
-                            } catch
-                            {
-                                Error($"invalid translation line \"{line}\".");
-                                continue;
-                            }
-                            if (languageSourceData.ContainsTerm(term))
-                            {
-                                languageSourceData.RemoveTerm(term);
-                                Warning($"official game term {term} was overwritten with \"{text}\"");
-                            }
-                            languageSourceData.AddTerm(term).Languages[languageIndex] = text;
-                        }
-                    }
-            }
-        }
-
         internal static bool Load(UnityModManager.ModEntry modEntry)
         {
             try
             {
                 Logger = modEntry.Logger;
-
-                LoadTranslations();
 
                 var harmony = new Harmony(modEntry.Info.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
@@ -78,11 +34,6 @@ namespace SolastaUIUpdates
             }
 
             return true;
-        }
-
-        internal static void ModEntryPoint()
-        {
-
         }
     }
 }
